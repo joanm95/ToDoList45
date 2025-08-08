80ea63d5c40ed9ebf8115e8f5114ffde2cfc1e7f
Joan <joanm95@gmail.com>
2025-08-08 12:56:00 -0400

    Initial Commit

---
+++ ToDoList45/ToDoList45App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList45App.swift
+//  ToDoList45
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList45App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
